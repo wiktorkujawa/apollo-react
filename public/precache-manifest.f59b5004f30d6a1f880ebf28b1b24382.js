19e86b8978dbf9e638c6e2750a9a762af3cbf240
Wiktor Kujawa <wiktorkujawa@onet.pl>
2020-05-10 12:56:59 +0200

    Finished app

---
+++ public/precache-manifest.f59b5004f30d6a1f880ebf28b1b24382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763413900c87eeef4e7946ade50dead0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26488ecacd3e94a6c08e",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0c361c3ab59142e91d78",
+    "url": "/static/js/2.cbe203b5.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.cbe203b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26488ecacd3e94a6c08e",
+    "url": "/static/js/main.ce6fc16a.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
